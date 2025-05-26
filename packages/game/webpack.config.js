0252eadc02824b0f10da79332cf082a1db5b1a4b
sergeysavin <serzz1990@gmail.com>
2025-05-27 02:40:35 +0400

    deploy

--- packages/game/webpack.config.js
+++ packages/game/webpack.config.js
@@ -75,7 +75,7 @@ module.exports = {
     })
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, './dist')
   },
   devServer: {
