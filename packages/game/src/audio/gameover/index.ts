ea85ab62a7a1cc651c3029d95588f2d085576012
sergeysavin <serzz1990@gmail.com>
2025-06-09 23:16:12 +0400

    upd sounds

--- packages/game/src/audio/gameover/index.ts
+++ packages/game/src/audio/gameover/index.ts
@@ -3,8 +3,6 @@ import { Audio } from '../../types';
 import gameoverMusicUrl from './assets/gameover1.mp3';
 import gameoverMusicUrl2 from './assets/gameover2.mp3';
 import gameoverMusicUrl3 from './assets/gameover3.mp3';
-import gameoverMusicUrl4 from './assets/gameover4.mp3';
-import gameoverMusicUrl5 from './assets/gameover5.mp3';
 
 export const GameoverAudio: Audio.Asset = {
   url: gameoverMusicUrl,
@@ -27,34 +25,16 @@ export const GameoverAudio3: Audio.Asset = {
   type: Audio.Type.Effect
 };
 
-export const GameoverAudio4: Audio.Asset = {
-  url: gameoverMusicUrl4,
-  key: 'gameover_music4',
-  volume: 1,
-  type: Audio.Type.Effect
-};
-
-export const GameoverAudio5: Audio.Asset = {
-  url: gameoverMusicUrl5,
-  key: 'gameover_music5',
-  volume: 1,
-  type: Audio.Type.Effect
-};
-
 const gameoverAudio = [
   GameoverAudio,
   GameoverAudio2,
   GameoverAudio3,
-  GameoverAudio4,
-  GameoverAudio5
 ];
 
 export const preloadGameoverAudio = (scene: Phaser.Scene) => {
   AudioService.preloadAsset(scene, GameoverAudio);
   AudioService.preloadAsset(scene, GameoverAudio2);
   AudioService.preloadAsset(scene, GameoverAudio3);
-  AudioService.preloadAsset(scene, GameoverAudio4);
-  AudioService.preloadAsset(scene, GameoverAudio5);
 };
 
 export const playGameoverAudio = (scene: Phaser.Scene) => {
